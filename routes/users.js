d94c8af9816dd49318a1622fd4b10d48f57db74d
Pongtawan Kaenprom <pongtawan2541@gmail.com>
2020-10-23 12:11:03 +0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,11 @@
 var express = require("express");
 var router = express.Router();
 
+const users = [
+  { username: "tan", password: "12345678" },
+  { username: "phet", password: "12345678" },
+];
+
 router.get("/register", function (req, res, next) {
   res.render("register");
 });
@@ -12,6 +17,11 @@ router.post("/register", function (req, res, next) {
 
 router.get("/login", function (req, res, next) {
   res.render("login");
+});
+
+router.post("/login", function (req, res, next) {
+  // Authen
+  const username = req.body.username;
 });
 
 module.exports = router;
